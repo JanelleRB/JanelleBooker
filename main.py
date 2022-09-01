fb9eabaa179131af97bcb270e67b37fddd752045
Janelle Booker <jrb29857@uga.edu>
2022-09-01 00:12:08 -0400

    adds website index.html file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# <!DOCTYPE html
-# <html>
-# <head>
-# <JanelleBooker>INFO2000</JanelleBooker>
-# </head>
-# <body>
+ <!DOCTYPE html
+ <html>
+ <head>
+ <JanelleBooker>INFO2000</JanelleBooker>
+ </head>
+ <body>
 
-# <h1>Exercise 3</h1>
-# <p>Fun Fact I have a hermit crab.</p>
+ <h1>Exercise 3</h1>
+ <p>Fun Fact I have a hermit crab.</p>
 
-# </body>
-# </html>
+ </body>
+ </html>
